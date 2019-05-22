b6a1ce13bf00c8fe40dd16d3a2528b6ed23893c1
Mathias <mathiasminacapilli@gmail.com>
2019-05-22 14:20:17 -0300

    First commit of the full project

---
+++ public/js/index.js
@@ -0,0 +1,85 @@
+/* ========================= */
+/* ========Functions======== */
+/* ========================= */
+
+/* Animate a number giving to the function the number of child
+you want to animeta and the final number of the animation. */
+function animateNumber(child, numberp) {
+    // how many decimal places allows
+    var decimal_places = 1;
+    var decimal_factor = decimal_places === 0 ? 1 : Math.pow(10, decimal_places);
+    $('#knowledge li:nth-child('+child+') p').animateNumber({number: numberp * decimal_factor,
+        numberStep: function(now, tween) {
+            var floored_number = Math.floor(now) / decimal_factor,
+                target = $(tween.elem);
+            if (decimal_places > 0) {
+                // force decimal places even if they are 0
+                floored_number = floored_number.toFixed(decimal_places);
+                // replace '.' separator with ','
+                floored_number = floored_number.toString();
+            }
+            target.text(floored_number);
+        }
+    }, 2200
+    );
+}
+
+/* 
+
+*/
+$(document).ready(function(){
+
+    /* Aside Control */
+    let navigation_state = 0;
+    var aside = document.getElementById('aside'); //Icono de flecha
+    aside.addEventListener('click', function(e) {
+        let list = document.getElementById('navigation-list');
+        if(navigation_state == 0) {
+            list.style.left = '0';
+            aside.style.left = '30vw';
+            navigation_state = 1;
+        }
+        else { //state = 1
+            list.style.left = '-30vw';
+            aside.style.left = '0';
+            navigation_state = 0;
+        }
+    });
+  
+    /* Animate Numbers of Knowledge Control */
+    animateNumber(1, 9.0);
+    animateNumber(2, 8.4);
+    animateNumber(3, 5.0);
+    animateNumber(4, 7.5);
+    animateNumber(5, 6.0);
+    animateNumber(6, 4.6);
+    animateNumber(7, 8.0);
+    animateNumber(8, 1.0);
+
+    /* 
+    ------------------------------------------
+    ---------------Cash the DOM---------------
+    ------------------------------------------
+    */
+    const contact_form = document.querySelector('#contact-form');
+
+    /* 
+    -------------------------------------------------
+    ---------------Auxiliary Functions---------------
+    -------------------------------------------------
+    */
+    function showContactForm() {
+        alert("presionado");
+    }
+
+    /* 
+    ---------------------------------------------
+    ---------------Event listeners---------------
+    ---------------------------------------------
+    */
+   document.querySelector('#contact-btn').addEventListener('click', showContactForm);
+
+});
+
+
+
